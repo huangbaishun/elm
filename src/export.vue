6840496493c6e16e6012dca6e0465612f8a38762
go it <43737154+huangbaishun@users.noreply.github.com>
2019-11-19 16:41:47 +0800

    Update export.vue

--- src/export.vue
+++ src/export.vue
@@ -87,3 +87,113 @@ export default {
   }
 };
 </script>
+
+
+兼容IE
+
+<template>
+  <a 
+  class="el-button el-button--default" :class="disabled?'is-disabled':''" 
+  :href="url" :download="name">
+    <slot>{{ btnName }}</slot>
+  </a>
+</template>
+<script>
+export default {
+  props: {
+    query: {
+      type: Object,
+      default: function() {
+        return {};
+      }
+    },
+    action: String,
+    method: {
+      type: String,
+      default: "get"
+    },
+    type: String,
+    fileName: {
+      type: String,
+      default: `导出数据.xlsx`
+    },
+    disabled: Boolean,
+    btnName: {
+      type: String,
+      default: '导出'
+    },
+  },
+  computed: {
+    url: function() {
+      let action = this.action;
+      let query = this.query;
+      let cache = [];
+      Object.keys(query).map(function(item) {
+        if (query[item] != undefined) {
+          cache.push(
+            `${encodeURIComponent(item)}=${encodeURIComponent(query[item])}`
+          );
+        }
+      });
+      if (action && action[0] !== "/") action = `/${action}`;
+      // return process.env.VUE_APP_BASEURL + ":8086" + action+'?'+cache.join('&');
+      return action + "?" + cache.join("&");
+    },
+    name: function() {
+      let names = this.fileName.split(".");
+      return `${names[0]}_${new Date()
+        .toLocaleDateString()
+        .replace(/\//g, "-")}.${names[names.length - 1]}`;
+    }
+  },
+  methods: {
+    getFile: function() {
+      let query = this.query;
+      this.request({
+        url: this.action,
+        method: this.method,
+        data: query,
+        params: query,
+        responseType: "blob"
+      }).then(res => {
+        if (res.type === "application/json") {
+          this.handleError(res);
+        } else {
+          this.downloadFile(res);
+        }
+      });
+    },
+    handleError: function(blob) {
+      let reader = new FileReader();
+      let that = this;
+      reader.onload = function() {
+        let msg = JSON.parse(reader.result);
+        that.$message({
+          type: "error",
+          showClose: true,
+          message: msg.msg
+        });
+      };
+      reader.readAsText(blob);
+    },
+    downloadFile: function(blob) {
+      let downloadUrl = URL.createObjectURL(blob);
+      let a = document.createElement("a");
+      let names = this.fileName.split(".");
+      a.href = downloadUrl;
+      a.download = `${names[0]}_${new Date()
+        .toLocaleDateString()
+        .replace(/\//g, "-")}.${names[names.length - 1]}`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(downloadUrl);
+    }
+  }
+};
+</script>
+<style scoped>
+.is-disabled {
+  pointer-events: none;
+}
+</style>
